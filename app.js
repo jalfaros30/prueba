921b314a47e5213b6cd3fe5ca5c8af167b127caa
jalfaros30 <jalfaros.ome@gmail.com>
2021-01-11 17:14:41 -0600

    cambio

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-/*
-const http = require('http');
-const port = process.env.PORT|| 3000;
-// se utiliza el método create server
-const server = http.createServer((req, res) => {
-  res.json({'nombre':'jorge'}); 
-});
-
-
-server.listen(port, () => {
- console.log('El servidor esta escuchando peticiones en localhost');
-});
-
-*/
 
 $(document).ready(function() {
   $("#magnify").click(function() {
