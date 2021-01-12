3703f149a5607cea8113f5bd5860372adea54793
jalfaros30 <jalfaros.ome@gmail.com>
2021-01-11 19:23:04 -0600

    cambio_estructura

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ $(document).ready(function() {
   });
 
   $(".container").focusout(function() {
-    $(".search").removeClass("focus");
-    $("#magnify").removeClass("clicked");
+    //$(".search").removeClass("focus");
+    //$("#magnify").removeClass("clicked");
   });
 });
+
+
+const id = document.querySelector('#icon-search');
+const search = document.querySelector('#search');
+
+search.addEventListener('change',() => {
+  busq_clausula = `#clausula${search.value}`;
+  location.href= busq_clausula;
+  document.querySelector('#search').value = '';
+});
+
+
+/*
+function redireccionar(){
+  window.locationf="http://www.cristalab.com";
+} 
+setTimeout ("redireccionar()", 5000);
+
+*/
 
